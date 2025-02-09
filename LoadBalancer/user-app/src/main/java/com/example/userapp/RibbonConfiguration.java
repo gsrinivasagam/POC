8711a60ac6021db9f697948077d898bbb2b74dad
gsrinivasagam <33370296+gsrinivasagam@users.noreply.github.com>
2025-02-09 14:44:54 +0530

    Add files via upload

---
+++ LoadBalancer/user-app/src/main/java/com/example/userapp/RibbonConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.userapp;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AvailabilityFilteringRule;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+public class RibbonConfiguration {
+
+
+    @Bean
+    public IPing ribbonPing(){
+        return new PingUrl();
+    }
+
+}
